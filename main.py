b54fefce654a1c17084c528c6f4f9de717121973
VantaTree <82938904+VantaTree@users.noreply.github.com>
2022-04-21 02:39:50 +0000

    added uptime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 from os import listdir
+import uptime
 from discord.ext import commands
 from config.config import *
 
@@ -10,4 +11,5 @@ client = commands.Bot(command_prefix=PREFIXES, intents=intents)
 client.remove_command('help')
 [client.load_extension(f'cogs.{filename[:-3]}') for filename in listdir('./cogs') if filename.endswith('.py')]
 
+uptime.keep_alive()
 client.run(TOKEN) #Bot to be used in a single Server(Configured in config.py)
